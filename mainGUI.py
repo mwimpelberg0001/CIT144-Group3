e3422e3033266cc5a87c3c8640b2faf259aba22e
Matt Wimpelberg <mwimpelberg0001@kctcs.edu>
2015-11-25 20:10:22 -0600

    Notes
    removed a section I commented out for testing purposes. Didnt want to
    lose the typing till I was sure it looked good. As it did, I removed the
    old work out.  Further added notes for functions to be added

--- mainGUI.py
+++ mainGUI.py
@@ -4,9 +4,11 @@ class mainGUI():
     def __init__(self):
         window = Tk() #create window
         window.title("Python Classroom ATM") #set window title
-        canvas = Canvas(window, width = 300, height = 150)
+        #sets background size
+        canvas = Canvas(window, width = 300, height = 150) 
         canvas.pack()
 
+        #places buttons into window, links to definitions below
         btDeposit = Button(window, text = "Deposit", command = self.deposit).place(x = 20, y = 20)
         btWithdraw = Button(window, text = "Withdraw", command = self.withdraw).place(x = 20, y = 60)
         btQuickWithdraw = Button(window, text = "Quick Withdraw $40", command = self.quickWithdraw).place(x = 20, y = 100)
@@ -14,33 +16,32 @@ class mainGUI():
         btTransfer = Button(window, text = "Transfer Funds", command = self.transfer).place(x = 200, y = 60)
         btLogout = Button(window, text = "Log Out", command = self.logout).place(x = 200, y = 100)
 
-#        label1.grid(row = 1, column = 1)
-#        btDeposit.grid(row = 2, column = 1)
-#        btWithdraw.grid(row = 3, column = 1)
-#        btQuickWithdraw.grid(row = 4, column = 1)
-#        btCheckBalance.grid(row = 2, column = 2)
-#        btTransfer.grid(row = 3, column = 2)
-#        btLogout.grid(row = 5, column = 2)
-
 
     def deposit(self):
+        #call deposit window menu
         return
 
     def withdraw(self):
+        #to call withdraw window menu
         return
 
     def quickWithdraw(self):
+        #automatically withdraws 40 from balance
         return
 
     def checkBalance(self):
+        #returns balance amount
         return
 
     def transfer(self):
+        #pulls transfer window
         return
 
     def logout(self):
+        #closes program
         return
 
+#only purpose is to call mainGUI for viewing
 mainGUI()
 
     

9895d5c32b09165c4d3e786790ceda090da03763
Matt Wimpelberg <mwimpelberg0001@kctcs.edu>
2015-11-25 20:05:28 -0600

    Main GUI
    Cleaned up GUI, looks "Better" now

--- mainGUI.py
+++ mainGUI.py
@@ -1,28 +1,26 @@
 from tkinter import *
 
 class mainGUI():
-    def __init__(self,userID):
-        self.__userID = userID
+    def __init__(self):
         window = Tk() #create window
-        window.title("Please Selection from one of the following options") #set window title
+        window.title("Python Classroom ATM") #set window title
+        canvas = Canvas(window, width = 300, height = 150)
+        canvas.pack()
 
-        frame1 = Frame(window)
-        frame1.pack()
-        label1 = Label(frame1, text = "Please Selection from one of the following options")
-        btDeposit = Button(frame1, text = "Deposit", command = self.deposit)
-        btWithdraw = Button(frame1, text = "Withdraw", command = self.withdraw)
-        btQuickWithdraw = Button(frame1, text = "Quick Withdraw $40", command = self.quickWithdraw)
-        btCheckBalance = Button(frame1, text = "Check Balance", command = self.checkBalance)
-        btTransfer = Button(frame1, text = "Transfer Funds", command = self.transfer)
-        btLogout = Button(frame1, text = "Log Out", command = self.logout)
+        btDeposit = Button(window, text = "Deposit", command = self.deposit).place(x = 20, y = 20)
+        btWithdraw = Button(window, text = "Withdraw", command = self.withdraw).place(x = 20, y = 60)
+        btQuickWithdraw = Button(window, text = "Quick Withdraw $40", command = self.quickWithdraw).place(x = 20, y = 100)
+        btCheckBalance = Button(window, text = "Check Balance", command = self.checkBalance).place(x = 200, y = 20)
+        btTransfer = Button(window, text = "Transfer Funds", command = self.transfer).place(x = 200, y = 60)
+        btLogout = Button(window, text = "Log Out", command = self.logout).place(x = 200, y = 100)
 
-        label1.grid(row = 1, column = 1)
-        btDeposit.grid(row = 2, column = 1)
-        btWithdraw.grid(row = 3, column = 1)
-        btQuickWithdraw.grid(row = 4, column = 1)
-        btCheckBalance.grid(row = 2, column = 2)
-        btTransfer.grid(row = 3, column = 2)
-        btLogout.grid(row = 5, column = 2)
+#        label1.grid(row = 1, column = 1)
+#        btDeposit.grid(row = 2, column = 1)
+#        btWithdraw.grid(row = 3, column = 1)
+#        btQuickWithdraw.grid(row = 4, column = 1)
+#        btCheckBalance.grid(row = 2, column = 2)
+#        btTransfer.grid(row = 3, column = 2)
+#        btLogout.grid(row = 5, column = 2)
 
 
     def deposit(self):
@@ -43,8 +41,7 @@ class mainGUI():
     def logout(self):
         return
 
-name = "Keith"
-mainGUI(name)
+mainGUI()
 
     
 

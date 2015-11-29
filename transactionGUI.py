e511985b26449567b11628342ee4f2759b6bdea8
Matt Wimpelberg <mwimpelberg0001@kctcs.edu>
2015-11-29 09:25:33 -0600

    Transaction GUI added
    Added a GUI that would be a funnel from the deposit, withdraw and quick
    withdraw GUI.  Those would pass an amount and relevant information to
    this GUI.  This will allow user to choose checking or savings for the
    transaction to occur to.  Variables to pass are not setup yet.

---
+++ transactionGUI.py
@@ -0,0 +1,40 @@
+#this will take input of two variables and adjust balances of accounts
+#based on input. Current condition is only designed for viewing GUI. Future
+#transactions for deposit, withdraw, and quick withdraw will use this
+#and pass the desired amounts for selection from/to checking and savings.
+
+from tkinter import *
+
+
+
+class quickGUI():
+    def __init__(self):
+        window = Tk() #create window
+        window.title("Python Classroom ATM") #set window title
+        #sets background size
+        canvas = Canvas(window, width = 300, height = 150) 
+        canvas.pack()
+
+        #places buttons into window, links to definitions below
+        btChecking = Button(window, text = "From Checking", command = self.checking).place(x = 20, y = 20)
+        btSavings = Button(window, text = "From Savings", command = self.savings).place(x = 20, y = 60)
+        btMainMenu = Button(window, text = "Return to Main menu", command = self.mainMenu).place(x = 100, y = 125)
+
+
+    def checking(self):
+        #removes X from balance
+        return
+
+    def savings(self):
+        #removes X from balance
+        return
+
+    def mainMenu(self):
+        #returns to mainGUI
+        return
+
+#only purpose is to call mainGUI for viewing
+quickGUI()
+
+    
+

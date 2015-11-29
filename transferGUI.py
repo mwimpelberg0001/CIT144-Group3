1e477f3f47b7c39c05283015eb65fe798ae35916
Matt Wimpelberg <mwimpelberg0001@kctcs.edu>
2015-11-29 10:12:01 -0600

    Transfer non-functioning
    Going to take a look at an ATM sometime today or tomorrow to get some
    ideas on how to set this up.  Have a couple ideas, but not sure how to
    structure it for user friendliness.  Ideas range from having a button
    that flips savings and checking to just placing a text entry field and
    then two buttons that say "to savings from checking" or "to checking
    from savings"

---
+++ transferGUI.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+class transferGUI():
+    def __init__(self):
+        window = Tk() #create window
+        window.title("Python Classroom ATM") #set window title
+        
+        

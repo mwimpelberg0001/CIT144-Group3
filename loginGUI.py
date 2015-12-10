2f20c34871f6282bfb076a3f5a87231695b70fe2
Matt Wimpelberg <mwimpelberg0001@kctcs.edu>
2015-12-09 19:23:47 -0600

    failed password
    Added textVar to pop in loginGUI if password/user failed

--- loginGUI.py
+++ loginGUI.py
@@ -21,12 +21,15 @@ class loginGUI:
         self.userPass = StringVar() #define variable to store password
         passwordID = Entry(frame1, textvariable = self.userPass) #Enter userPass
         btSubmit = Button(frame1, text = "Login", command = self.submitButton) #Submit button returns info
+        self.badPassword = StringVar()
+        label3 = Label(frame1, textvariable = self.badPassword)
         #design layout of login GUI
         label1.grid(row = 1, column = 1) 
         label2.grid(row = 2, column = 1)
         entryID.grid(row = 1, column = 2)
         passwordID.grid(row = 2, column = 2)
         btSubmit.grid(row = 3, column = 3)
+        label3.grid(row = 4, column = 2)
         
         self.window.mainloop() #needed here now, since we will import mainGUI functions after a succesful login
 
@@ -50,7 +53,11 @@ class loginGUI:
                     inputfile.close()
                     self.window.destroy()
                     mainGUI(name, activeUser) #fire up the main GUI
+                    return
                 else:
-                    print ("Wrong password") #Password didnt match user given
+                    self.badPassword.set("Invalid Password")
+                    return
+
+        
 
 loginGUI()
